f1b4e681e80fdf9547765f53d9aa2a8b6efa2d52
chaewon-moana <dreamytaurus508@gmail.com>
2024-05-12 19:57:16 +0900

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by cho on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
